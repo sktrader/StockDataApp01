28a67829fe5559b073a43c8966799ad70457eb4a
algowebpro <kswapnil55@gmail.com>
2024-10-06 10:37:16 +0530

    stock info entity

---
+++ src/main/java/com/example/entity/StockInfo.java
@@ -0,0 +1,13 @@
+package com.example.entity;
+
+
+public class StockInfo {
+	
+	private int id;
+	//NSE | BSE | BOTH
+	private String exch;
+
+}
+
+/*
+ * Requiremnt is to Update the exch inside database NSE or BSE or Both and create a frontend dropdown */
